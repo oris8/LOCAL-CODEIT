f9b2b3dbeeb59b567293d82b7d3261dd4fa30095
eprnf <eprnf21@gmail.com>
2024-10-18 16:23:46 +0900

    ✅ test: test

--- packages/shared-backend/amplify/backend.ts
+++ packages/shared-backend/amplify/backend.ts
@@ -2,14 +2,9 @@ import { reservationConflictHandler } from "./function/reservationConflictHandle
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
-import { Tags } from "aws-cdk-lib";
 
 const backend = defineBackend({
   auth,
   data,
   reservationConflictHandler,
 });
-
-const tags = Tags.of(backend.stack);
-
-tags.add("202410_InternA", "202410_InternA");
