2b0cb71ad6df774a81a7c2079df09fc76cdb9e0b
eprnf <eprnf21@gmail.com>
2024-10-18 16:24:30 +0900

    ✅ test: test

--- packages/shared-backend/amplify/backend.ts
+++ packages/shared-backend/amplify/backend.ts
@@ -3,7 +3,7 @@ import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
   reservationConflictHandler,
